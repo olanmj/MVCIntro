56331dc6237bec9926931ceae75866241dafbb95
olanmj <olanm@stockton.edu>
2016-02-16 14:16:07 -0500

    Sample repo

---
+++ MVCIntro/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntro.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index(int id)
+        {
+            ViewBag.Value = id;
+            return View();
+        }
+    }
+}
---
+++ MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult  Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.AnotherMessage = "This is my application";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

dae805796ae43f44d068e8d4c63dbbd8ea5f0b7f
olanmj <olanm1@msn.com>
2016-02-17 22:23:18 -0500

    C# examples added

--- MVCIntro/Controllers/HelloController.cs
+++ MVCIntro/Controllers/HelloController.cs
@@ -14,5 +14,16 @@ namespace MVCIntro.Controllers
             ViewBag.Value = id;
             return View();
         }
+
+        public ActionResult Misc()
+        {
+            string[] stringArray = { "apple", "orange", "banana" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDictionary = new Dictionary<string, int> { { "apple", 4131 }, { "orange", 3107 }, { "banana", 4011 } };
+            ViewBag.Array = stringArray;
+            ViewBag.List = intList;
+            ViewBag.Dictionary = myDictionary;
+            return View();
+        }
     }
 }

fb135a854086171bc1fab083e1afec5aac1a13c4
olanmj <olanm1@msn.com>
2016-02-25 00:37:14 -0500

    Model validation added

---
+++ MVCIntro/Migrations/201602250512148_validation.cs
@@ -0,0 +1,22 @@
+namespace MVCIntro.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class validation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Products", "Description", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Category", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Category", c => c.String());
+            AlterColumn("dbo.Products", "Description", c => c.String());
+            AlterColumn("dbo.Products", "Name", c => c.String());
+        }
+    }
+}
--- MVCIntro/Models/Product.cs
+++ MVCIntro/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace MVCIntro.Models
@@ -7,9 +8,14 @@ namespace MVCIntro.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Product name required")]
+        [StringLength(20, ErrorMessage = "2 - 20 characters needed", MinimumLength = 2)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Descrition required")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Price required")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Category required")]
         public string Category { set; get; }
     }
 
